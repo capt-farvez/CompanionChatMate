3db4ad52f05fe755a1dfc6dec0fc70d3f7f5c95a
Anowar Hossen Farvez <farvez165@gmail.com>
2023-10-12 00:16:03 +0600

    setup urls

--- chatmate/urls.py
+++ chatmate/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from home.views import home, chat_clear
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('chat/clear', chat_clear, name="clear_chat"),
 ]
